556db11fc0932a64fbc18029de2a210654ca0441
heitorcalaca <heitor.calaca@gmail.com>
2023-06-21 19:08:06 -0300

    ver 1

--- app/page.js
+++ app/page.js
@@ -2,50 +2,44 @@
 import { GiCow } from 'react-icons/gi';
 import TabelaMatriz from '@/components/tabela_matiz';
 import FormularioMatriz from '@/components/formulario_matriz';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { Provider } from 'react-redux';
-import store from '@/redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleChangeAction } from '@/redux/reducer';
 
-const queryClient = new QueryClient();
+export default function Home() {
 
-export const metadata = {
-  title: 'Controle de Gado',
-  description: 'Desenvolvido por Calaça Digital',
-};
-
-export default function Home({ Component, pageProps }) {
-  const [visible, setVisible] = useState(false);
+  const visible = useSelector((state) => state.app.client.toggleForm)
+  const dispatch = useDispatch()
 
   const handler = () => {
-    setVisible(!visible);
+    dispatch(toggleChangeAction())
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <section>
-          <main className='py-5'>
-            <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Controle de Gado</h1>
 
-            <div className='container mx-auto flex justify-between py-5 border-b'>
-              <div className='left flex gap-3'>
-                <button onClick={handler} className='flex bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-slate-400'>
-                  Nova Matriz <span className='pl-2'> <GiCow size={23} /> </span>
-                </button>
-              </div>
 
-              {/* collapsible form */}
-              {visible && <FormularioMatriz />}
-            </div>
+    <section>
 
-            {/* table */}
-            <div className='container mx-auto'>
-              <TabelaMatriz />
-            </div>
-          </main>
-        </section>
-      </Provider>
-    </QueryClientProvider>
+      <main className='py-5'>
+        <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Controle de Gado</h1>
+
+        <div className='container mx-auto flex justify-between py-5 border-b'>
+          <div className='left flex gap-3'>
+
+            <button onClick={handler} className='flex bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-slate-400'>
+              Nova Matriz <span className='pl-2'> <GiCow size={23} /> </span>
+            </button>
+          </div>
+
+        </div>
+        {/* collapsible form */}
+        {visible && <FormularioMatriz />}
+
+        {/* table */}
+        <div className='container mx-auto'>
+          <TabelaMatriz />
+        </div>
+      </main>
+    </section>
+
   );
 }
